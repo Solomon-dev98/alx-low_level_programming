4aa246b374a782cb885783fb14062e98de15b91d
Solomon-dev98 <asapnino01@gmail.com>
2023-08-11 22:46:50 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: 0 when succesful
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	for (a = 'A'; a <= 'Z'; a++)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
