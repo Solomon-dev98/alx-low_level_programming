0b17745fc85666a2f76031fa0ce8761fad1b69ea
Solomon-dev98 <asapnino01@gmail.com>
2023-09-05 15:14:06 -0700

    task 6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,75 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * strtow - a function that splits a string into words.
+ * @str: a parameter.
+ * count_words - a function that count words.
+ * Return: NULL.
+ */
+
+char count_words(char *str)
+{
+	int count = 0;
+	int in_word = 0;
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ' && !in_word)
+		{
+			count++;
+			in_word = 1;
+		}
+		else if (str[i]  == ' ')
+		{
+			in_word = 0;
+		}
+	}
+
+	return (count);
+}
+
+char **strtow(char *str)
+{
+	char **words;
+	int word_in = 0, word_s = 0, in_word = 0, i, j, num_w = count_words(str), wl;
+
+	if (str == NULL || *str == '\0')
+	{
+		return (NULL);
+	}
+	words = (char **)malloc((num_w + 1) * sizeof(char *));
+	if (words == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ' && !in_word)
+		{
+			word_s = i;
+			in_word = 1;
+		}
+		else if ((str[i] == ' ' || str[i + 1] == '\0') && in_word)
+		{
+			wl = (str[i] == ' ') ? i - word_s : i - word_s + 1;
+			words[word_in] = (char *)malloc((wl + 1) * sizeof(char));
+			if (words[word_in] == NULL)
+			{
+				for (j = 0; j < word_in; j++)
+				{
+					free(words[j]);
+				}
+				free(words);
+				return (NULL);
+			}
+			strncpy(words[word_in], str + word_s, wl);
+			words[word_in][wl] = '\0';
+			word_in++;
+			in_word = 0;
+		}
+	}
+	words[num_w] = NULL;
+	return (words);
+}
