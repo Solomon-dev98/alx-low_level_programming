9c7f823aa9d24c13c9f5670e735a194e59754e1d
Solomon-dev98 <asapnino01@gmail.com>
2023-10-28 04:21:56 +0100

    add function that prints the name of program, argc,argv project

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function that prints the name of a program.
+ * @argc: a variable.
+ * @argv: an array of pointers to string.
+ * Return: 0 on success.
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
+
