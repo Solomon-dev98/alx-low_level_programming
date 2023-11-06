6ba2a2e9ccf67eab9b5c26168b717c5d735f2464
Solomon-dev98 <asapnino01@gmail.com>
2023-11-06 15:54:32 +0100

    add file containing a structure with tag dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef D0G_H
+#define DOG_H
+
+/**
+ * struct dog - a desription of user-defined data type dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
