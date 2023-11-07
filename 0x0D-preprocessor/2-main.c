8adf1917e26e8cf994cbe6b163729dfcb189c8d4
Solomon-dev98 <asapnino01@gmail.com>
2023-11-07 07:24:30 +0100

    add a program that prints the name of the file it was compiled from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,11 +3,11 @@
 /**
  * main - a function that prints name of its file.
  * Return: returns 0 on success
- */ 
+ */
 int main(void)
 {
 	/*Prints name of the file*/
 
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
