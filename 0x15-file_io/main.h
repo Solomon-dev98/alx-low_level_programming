eafa8f0e6b51958089d7475058ef7ef88691fd3a
Solomon-dev98 <asapnino01@gmail.com>
2023-11-28 05:47:01 +0100

    feature: added two prototypes for main function and void_stat function for program that copies the content of one file to another file.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,5 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
+void check_IO_stat(int stat, int fd, char *filename, char mode);
+int main(int argc, char *argv[]);
 
 #endif /* MAIN_H */
